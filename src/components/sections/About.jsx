6fac75104455e9e60526e545a659962825cd1b66
Jeff GBANZIALI <jeffgbanziali@MacBook-Pro-de-Jeff.local>
2025-02-06 02:42:49 +0100

    first commit

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,15 +1,63 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
+  const programmingLanguages = [
+    "JavaScript",
+    "Python",
+    "R",
+    "Java",
+    "C#",
+    "PHP",
+    "SQL",
+    "TypeScript",
+  ];
+
   const frontendSkills = [
     "React",
     "Vue",
     "TypeScript",
     "TailwindCSS",
-    "Svelte",
+    "HTML",
+    "CSS",
   ];
 
-  const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
+  const backendSkills = ["Node.js", "Django", "Spring Boot", ".NET", "Symfony"];
+
+  const databaseSkills = [
+    "MySQL",
+    "MongoDB",
+    "PostgreSQL",
+    "SQL Server",
+    "Oracle DB",
+  ];
+
+  const bigDataSkills = [
+    "Hadoop",
+    "Spark",
+    "Kafka",
+    "Hive",
+    "MapReduce",
+    "Cloudera",
+  ];
+
+  const aiSkills = ["TensorFlow", "Keras", "PyTorch", "Scikit-learn"];
+
+  const dataVisualizationTools = [
+    "Tableau",
+    "Power BI",
+    "Matplotlib",
+    "Seaborn",
+    "D3.js",
+  ];
+
+  const mobileSkills = [
+    "React Native",
+    "Flutter",
+    "Swift",
+    "Kotlin",
+    "Android Studio",
+    "Xcode",
+  ];
 
   return (
     <section
@@ -25,11 +73,33 @@ export const About = () => {
 
           <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-300 mb-6">
-              Passionate developer with expertise in building scalable web
-              applications and creating innovative solutions.
+              Developer specializing in the creation of scalable web solutions,
+              mobile applications and the exploitation of massive data. Eager to
+              conquer new challenges in Big Data and Artificial Intelligence, I
+              am ready to work on projects aimed at optimizing processes and
+              providing data-driven solutions, in order to help companies make
+              informed decisions.
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="grid grid-cols-2 md:grid-cols-2 gap-4">
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">
+                  {" "}
+                  Programming Languages
+                </h3>
+                <div className="flex flex-wrap gap-2">
+                  {programmingLanguages.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4"> Frontend</h3>
                 <div className="flex flex-wrap gap-2">
@@ -61,6 +131,85 @@ export const About = () => {
                   ))}
                 </div>
               </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4"> Mobile App</h3>
+                <div className="flex flex-wrap gap-2">
+                  {mobileSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">big Data </h3>
+                <div className="flex flex-wrap gap-2">
+                  {bigDataSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">AI </h3>
+                <div className="flex flex-wrap gap-2">
+                  {aiSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Visualization tools </h3>
+                <div className="flex flex-wrap gap-2">
+                  {dataVisualizationTools.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                <h3 className="text-xl font-bold mb-4">Database </h3>
+                <div className="flex flex-wrap gap-2">
+                  {databaseSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    "
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
 
@@ -69,37 +218,43 @@ export const About = () => {
               <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
               <ul className="list-disc list-inside text-gray-300 space-y-2">
                 <li>
-                  <strong> B.S. in Computer Science </strong> - XYZ University
-                  (2016-2020)
+                  <strong> Bachelor Engineering and Digital </strong> - Efrei
+                  Paris (2021-2024)
                 </li>
                 <li>
-                  Relevant Coursework: Data Structures, Web Development, Cloud
-                  Computing...
+                  <strong>
+                    {" "}
+                    Master's degree Artificial Intelligence and Big Data
+                  </strong>{" "}
+                  - Efrei Paris (2024-2026)
                 </li>
+             
               </ul>
             </div>
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
               <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
               <div className="space-y-4 text-gray-300">
                 <div>
-                  <h4 className="font-semibold">
+                  <h4 className="font-bold">
                     {" "}
-                    Software Engineer at ABC Corp (2020 - Present){" "}
+                    FullStack Developer (Internship) at FIDJI PARIS (JANUARY
+                    2024 - JUNE 2024){" "}
                   </h4>
-                  <p>
-                    Developed and maintained microservices for cloud-based
-                    applications.
+                  <p className="text-gray-400">
+                    Implementation of a mobile application for the Fiji store,
+                    implementation of site resources on the app and Update of
+                    user data.
                   </p>
                 </div>
-
                 <div>
-                  <h4 className="font-semibold">
+                  <h4 className="font-bold">
                     {" "}
-                    Intern at DEF Startups (2019){" "}
+                    Web Designer (internship) at SARL LES SHIBAS (January 2022 -
+                    March 2022){" "}
                   </h4>
-                  <p>
-                    Assisted in building front-end components and integration
-                    REST APIs
+                  <p className="text-gray-400">
+                    Updating the company's website, Database Management, Added
+                    new content, Risk analyzes.
                   </p>
                 </div>
               </div>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,4 +1,5 @@
 import { RevealOnScroll } from "../RevealOnScroll";
+import profileImage from "../../assets/my_profil.png";
 
 export const Home = () => {
   return (
@@ -7,31 +8,45 @@ export const Home = () => {
       className="min-h-screen flex items-center justify-center relative"
     >
       <RevealOnScroll>
-        <div className="text-center z-10 px-4">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-            Hi, I'm Pedro Tech
-          </h1>
+        <div className="text-center flex flex-row ">
+          <div className="text-center  z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
+              Hi, I'm Jeff GBANZIALI
+            </h1>
 
-          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-            I’m a full-stack developer who loves crafting clean, scalable web
-            applications. My goal is to build solutions that offer both
-            exceptional performance and a delightful user experience.
-          </p>
-          <div className="flex justify-center space-x-4">
-            <a
-              href="#projects"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
-            >
-              View Projects
-            </a>
+            <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
+              4th year student in Big Data and Artificial Intelligence, with a
+              solid background in digital engineering. My journey has allowed
+              the development of versatile skills in development web, mobile and
+              software, as well as expertise in analysis and exploitation of
+              massive data. I am motivated by the search for innovative
+              technological solutions, model creation powerful predictions and
+              optimization of business processes thanks to the power of data.
+              Available to collaborate on projects with high added value.
+            </p>
+            <div className="flex justify-center space-x-4">
+              <a
+                href="#projects"
+                className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+              >
+                View Projects
+              </a>
 
-            <a
-              href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+              <a
+                href="#contact"
+                className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
              hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
-            >
-              Contact Me
-            </a>
+              >
+                Contact Me
+              </a>
+            </div>
+          </div>
+          <div className=" overflow-hidden w-72 h-72 rounded-2xl text-center z-10 px-4">
+            <img
+              src={profileImage}
+              alt="my profil"
+              className="w-full h-full rounded-full object-cover"
+            />
           </div>
         </div>
       </RevealOnScroll>
