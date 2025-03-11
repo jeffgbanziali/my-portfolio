977222c5199742b29af808fdffa40478bdbcc4bf
Jeff GBANZIALI <jeffgbanziali@MacBook-Pro-de-Jeff.local>
2025-03-11 01:29:56 +0100

    my portfolio

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,13 +14,26 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Financial Data Analysis
+              </h3>
               <p className="text-gray-400 mb-4">
-                Scalable cloud infrastructure management with real-time
-                monitoring and automated scaling.
+                This project uses Apache Spark and PySpark to analyze large
+                amounts of financial data. It includes processing, analyzing,
+                and predicting the closing prices of stocks of several companies
+                (Apple, Tesla, Google, and Microsoft) using linear regression
+                models. An interactive interface was developed with Dash to
+                visualize trends and predictions.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                {[
+                  "Apache Spark / PySpark",
+                  "Python",
+                  "Dash & Plotly",
+                  "Pandas",
+                  "SQL / Parquet / CSV",
+                  "Machine Learning"
+                ].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -34,7 +47,7 @@ export const Projects = () => {
 
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://github.com/jeffgbanziali/Financial_Data_Analysis_.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
@@ -88,32 +101,38 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
+              <h3 className="text-xl font-bold mb-2">Fidji Store Mobile App</h3>
               <p className="text-gray-400 mb-4">
-                Full-stack e-commerce with modern UI, secure payment
-                integration, and customizable product inventory.
+                This mobile app allows you to manage an online store based on
+                WooCommerce. It offers users an intuitive interface to browse,
+                purchase products, track their orders and make secure payments
+                via Stripe.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
-                  (tech) => (
-                    <span
-                      key={tech}
-                      className="
+                {[
+                  "React Native",
+                  "WooCommerce API",
+                  "Stripe",
+                  "Redux",
+                  "Axios"
+                ].map((tech) => (
+                  <span
+                    key={tech}
+                    className="
                       bg-blue-500/10 text-blue-500 py-1 px-3 
                       rounded-full text-sm
                       transition
                       hover:bg-blue-500/20 hover:-translate-y-0.5
                       hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
                     "
-                    >
-                      {tech}
-                    </span>
-                  )
-                )}
+                  >
+                    {tech}
+                  </span>
+                ))}
               </div>
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://github.com/jeffgbanziali/Fidji-store.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
@@ -129,13 +148,30 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Flajoo - Social Network Mobile App
+              </h3>
               <p className="text-gray-400 mb-4">
-                Scalable chat platform supporting real-time messaging, presence,
-                and group chat features.
+                Flajoo is a modern and interactive social network designed to
+                offer an immersive experience to users who want to share their
+                moments with their community. It combines the best features of
+                current social platforms with unique innovations such as music
+                integration via Spotify, an intelligent virtual assistant and
+                advanced media file management.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                {[
+                  "React Native",
+                  "Node.js",
+                  "MongoDB",
+                  "Express",
+                  "Socket.io",
+                  "Spotify API",
+                  "Google API",
+                  "Facebook API",
+                  "Apple API",
+                  "TensorFlow"
+                ].map((tech, key) => (
                   <span
                     key={key}
                     className="
@@ -152,7 +188,7 @@ export const Projects = () => {
               </div>
               <div className="flex justify-between items-center ">
                 <a
-                  href="#"
+                  href="https://github.com/jeffgbanziali/FlajooApp.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
